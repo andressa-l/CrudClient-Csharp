41d71f3200127d899c79e5fb101de002ee35a285
tarde.cetafaju <tarde.cetafaju@PC03LAB2521>
2024-02-21 17:15:26 -0300

    Adicionar arquivos de projeto.

---
+++ CrudClient/Models/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudClient.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CrudClient.Models
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options):base(options)
+        {
+
+        }
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+}
+
---
+++ CrudClient/Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudClient.Models
+{
+    [Table("Cliente")]
+    public class Clientes
+    {
+        [Key]
+        //[Required]
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        [Display(Name ="Telefone")]
+        public string Telefone { get; set; }
+    }
+}
